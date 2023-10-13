50085d14d387540fc0b3c216f16d001d23871864
IsaacSls <147279446+IsaacSls@users.noreply.github.com>
2023-10-12 20:08:45 -0500

    Add files via upload
    Doing some coding exercises

---
+++ numberToAString.js
@@ -0,0 +1,17 @@
+
+// We need a function that can transform a number (integer) into a string.
+
+
+
+
+function integerToString(num){
+    return num.integerToString();
+}
+
+
+
+//// or ///
+ function integerToString(num){
+    return num + '';
+ }
+
